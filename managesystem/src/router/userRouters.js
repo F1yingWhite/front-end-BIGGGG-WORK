940e31df551f3ab463277f5d71f2f5bea9e27783
许一涵 <81723334@qq.com>
2024-06-02 11:54:14 +0800

    feat:login页面

--- managesystem/src/router/userRouters.js
+++ managesystem/src/router/userRouters.js
@@ -1,5 +1,18 @@
-const userRouters = [
+import { UserDashboard } from "../pages/user/UserDashboard";
+import { UserLogin } from "../pages/user/UserLogin";
 
+const userRouters = [
+  {
+    path: "login",
+    element: <UserLogin />,
+  },
+  {
+    path: "dashboard",
+    element: <UserDashboard />,
+    children: [
+
+    ]
+  },
 ]
 
 export default userRouters;
